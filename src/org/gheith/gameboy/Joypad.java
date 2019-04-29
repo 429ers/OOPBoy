f2cd343b70f31d2c1578cd3078a155a2bfb05ed6
incisivebox <maroni720@gmail.com>
2019-04-28 22:19:57 -0500

    Update Joypad.java

--- src/org/gheith/gameboy/Joypad.java
+++ src/org/gheith/gameboy/Joypad.java
@@ -4,11 +4,21 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Joypad implements KeyListener {
+	
+	private MMU mmu;
 
+	public Joypad(MMU mmu) {
+		this.mmu = mmu;
+	}
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
 		System.out.println("key hit");
+		int code = e.getKeyCode();
+		if (code == KeyEvent.VK_LEFT || code == KeyEvent.VK_RIGHT || code == KeyEvent.VK_UP || code == KeyEvent.VK_DOWN) {
+			int reg = mmu.readByte(0xFF00) & 0x2F;
+			mmu.writeByte(0x00FF, reg | 0x00);
+		}
 	}
 
 	@Override
