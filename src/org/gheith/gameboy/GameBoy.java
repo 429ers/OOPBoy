bda11602192f7a700004d241175e71397d3b2b57
incisivebox <maroni720@gmail.com>
2019-05-04 16:53:33 -0500

    save on close implemented

--- src/org/gheith/gameboy/GameBoy.java
+++ src/org/gheith/gameboy/GameBoy.java
@@ -4,6 +4,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.text.SimpleDateFormat;
@@ -156,8 +158,54 @@ public class GameBoy extends JFrame{
         this.setMenuBar(new MainMenuBar(this));
         this.pack();
         this.setTitle("GheithBoy");
-        this.setVisible(true);
-        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setVisible(true);	
+        this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        WindowListener listener = new WindowListener() {
+
+			@Override
+			public void windowActivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void windowClosed(WindowEvent e) {
+				// TODO Auto-generated method stub
+				mmu.cleanUp();
+			}
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void windowDeactivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void windowDeiconified(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void windowIconified(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void windowOpened(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+        	
+        };
+        this.addWindowListener(listener);
         mmu = new MMU(fileName);
         cpu = new CPU(mmu);
         ppu = new PPU(mmu, gbs);
