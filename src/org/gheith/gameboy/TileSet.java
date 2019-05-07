5901f9fe7f87176b92cc8eaa2f7bb0d7c8069a22
incisivebox <maroni720@gmail.com>
2019-05-06 21:42:10 -0500

    tilesets support both vram banks

--- src/org/gheith/gameboy/TileSet.java
+++ src/org/gheith/gameboy/TileSet.java
@@ -11,6 +11,14 @@ public class TileSet implements Serializable {
 	private static final long serialVersionUID = 4048349332307406439L;
 	private Map<Integer, Tile> tiles;
 	private boolean isSetOne;
+	private boolean isBankOne;
+	
+	
+	public TileSet(MMU memory, int startAddress, int numTiles, boolean isSetOne, boolean isBankOne) {
+		this(memory, startAddress, numTiles, isSetOne);
+		this.isBankOne = isBankOne;
+	}
+	
 	
 	public TileSet(MMU memory, int startAddress, int numTiles, boolean isSetOne) {
 		this.isSetOne = isSetOne;
@@ -25,7 +33,9 @@ public class TileSet implements Serializable {
 			tiles.put(tileNum, new Tile(tileBytes));
 			tileNum++;
 		}
+		this.isBankOne = true;
 	}
+	
 	
 	public Tile getTile(int tileNumber) {
 		return tiles.get(tileNumber);
